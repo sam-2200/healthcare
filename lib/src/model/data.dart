ffe32faabd88a74faca8caee5cb4ab36b70502e1
sam-2200 <54469358+sam-2200@users.noreply.github.com>
2020-11-16 04:08:04 +0530

    Add files via upload

---
+++ lib/src/model/data.dart
@@ -0,0 +1,62 @@
+final doctorMapList = [
+  {
+    "name": "Dr. Jhurani",
+    "type": "Orthiopadic",
+    "rating": 4.5,
+    "goodReviews": 79.2,
+    "totalScore": 93.2,
+    "satisfaction": 85.2,
+    "isfavourite": true,
+    "image": "assets/doctor.png",
+    "description":
+        "A doctor can be found in several settings, including public health organization, group practices and  hospitals They have some of the most diverse and challenging careears available and part of a universally well-respected profession",
+  },
+  {
+    "name": "Dr. Pushpendra",
+    "type": "Neurology",
+    "rating": 3.5,
+    "goodReviews": 93.2,
+    "totalScore": 72.2,
+    "satisfaction": 89.2,
+    "isfavourite": false,
+    "image": "assets/doctor_4.png",
+    "description":
+        "A doctor can be found in several settings, including public health organization, group practices and  hospitals They have some of the most diverse and challenging careears available and part of a universally well-respected profession",
+  },
+  {
+    "name": "DR. Sk Sharma",
+    "type": "Cardiologist",
+    "rating": 3.8,
+    "goodReviews": 88.2,
+    "totalScore": 93.94,
+    "satisfaction": 78.2,
+    "isfavourite": false,
+    "image": "assets/doctor_3.png",
+    "description":
+        "A doctor can be found in several settings, including public health organization, group practices and  hospitals They have some of the most diverse and challenging careears available and part of a universally well-respected profession",
+  },
+  {
+    "name": "Dr. Shruti Goyal",
+    "type": "Psycologist",
+    "rating": 3.5,
+    "goodReviews": 12.2,
+    "totalScore": 75.2,
+    "satisfaction": 84.2,
+    "isfavourite": true,
+    "image": "assets/doctor_1.png",
+    "description":
+        "A doctor can be found in several settings, including public health organization, group practices and  hospitals They have some of the most diverse and challenging careears available and part of a universally well-respected profession",
+  },
+  {
+    "name": "Dr. Payal",
+    "type": "Physician",
+    "rating": 2.5,
+    "goodReviews": 42.2,
+    "totalScore": 51.2,
+    "satisfaction": 74.2,
+    "isfavourite": true,
+    "image": "assets/doctor_1.png",
+    "description":
+        "A doctor can be found in several settings, including public health organization, group practices and  hospitals They have some of the most diverse and challenging careears available and part of a universally well-respected profession",
+  }
+];
